a0871f308ae7130dba7a4515688e9d7e74da1b26
lokki <44494883+luckylokki@users.noreply.github.com>
2022-04-13 15:03:56 +0300

    +

--- lesson_04/square.py
+++ lesson_04/square.py
@@ -4,7 +4,7 @@ if n <= 0:
     print('Это число не натуральное, попробуйте еще раз.')
 else:
     cnt = 1
-    print(n, end='    ')
-    while cnt**2 <= n:
-        print(cnt**2, end=' ')
+    print(n, end='        ')
+    while cnt ** 2 <= n:
+        print(cnt ** 2, end=' ')
         cnt += 1
