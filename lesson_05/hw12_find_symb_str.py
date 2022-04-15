e7ce7c10b1d2234305253a4866175ce060e64ad6
lokki <44494883+luckylokki@users.noreply.github.com>
2022-04-15 18:39:19 +0300

    -

---
+++ lesson_05/hw12_find_symb_str.py
@@ -0,0 +1,12 @@
+print('ДЗ 12. Поиск символа в строке')
+full_str = input('Введите строку: ')
+smbl = input('Введите один символ: ')
+s = -1
+cnt = 0
+
+while True:
+    s = full_str.find((smbl), s+1)
+    if s == -1:
+        break
+    cnt += 1
+print("Количество вхождений символа в строку: ", cnt )
