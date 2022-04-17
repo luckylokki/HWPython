5d48d155f7021f68a554a611c7f0888e43f73f0a
loki <mr.vadim.prokopenko@gmail.com>
2022-04-17 19:46:52 +0300

    hw13_start

--- lesson_06/hw13_remove_list_element.py
+++ lesson_06/hw13_remove_list_element.py
@@ -1,3 +1,8 @@
 print('ДЗ 13. Удаление элемента списка.')
 num_lst = input('Введите список чисел через пробел: ').split()
-print(num_lst, type(num_lst))
+k = int(input('Введите индекс элемента: '))
+if k > len(num_lst) or k < 0:
+    print('Введеный индекс выходит за границы списка, попробуйте еще раз')
+else:
+    print(k)
+    print(num_lst, type(num_lst))
