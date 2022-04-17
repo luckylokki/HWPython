3ad6b20ff84f77a744f3a3bc7de163d7efa5e47a
loki <mr.vadim.prokopenko@gmail.com>
2022-04-17 19:04:26 +0300

    hw15_done+

--- lesson_06/hw15_reverse_list.py
+++ lesson_06/hw15_reverse_list.py
@@ -1,6 +1,8 @@
-lst = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+print('ДЗ 15. Развернуть список.')
+lst = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 cnt = -1
+print('Стартовый список:', lst)
 for i in range(len(lst) // 2):
     lst[cnt], lst[i] = lst[i], lst[cnt]
     cnt -= 1
-print(lst)
+print('Развернутый список:', lst)
