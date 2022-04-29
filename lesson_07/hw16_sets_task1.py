ef3a70c0474769ad0908e5186c30dab74734f059
loki <mr.vadim.prokopenko@gmail.com>
2022-04-29 16:08:47 +0300

    hw16_done

---
+++ lesson_07/hw16_sets_task1.py
@@ -0,0 +1,7 @@
+from random import randint
+print('ДЗ 16. Задача на множества 1')
+lst = [randint(1,99) for _ in range(30)]
+unique = len(set(lst)) #решение одной строкой
+print('Дан список чисел:\n',lst)
+print('Количество чисел в списке всего:', len(lst))
+print('Количество уникальных чисел в списке:', unique)
