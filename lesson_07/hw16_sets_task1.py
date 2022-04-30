1d9112131330225cb4d679849735150800aa3690
loki <mr.vadim.prokopenko@gmail.com>
2022-04-30 15:42:22 +0300

    hw16_update

--- lesson_07/hw16_sets_task1.py
+++ lesson_07/hw16_sets_task1.py
@@ -2,7 +2,7 @@ from random import randint
 
 print('ДЗ 16. Задача на множества 1')
 lst = [randint(1, 99) for _ in range(30)]
-unique = len(set(lst))  # решение одной строкой
+unique = len(set(lst))
 print('Дан список чисел:\n', lst)
 print('Количество чисел в списке всего:', len(lst))
 print('Количество уникальных чисел в списке:', unique)
