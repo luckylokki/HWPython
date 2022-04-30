3a82e69cf37444791084f596995e5b231be59c66
loki <mr.vadim.prokopenko@gmail.com>
2022-04-30 15:41:47 +0300

    hw18_update

--- lesson_07/hw18_dictionary_task.py
+++ lesson_07/hw18_dictionary_task.py
@@ -1,6 +1,10 @@
 print('ДЗ 18. Задача на словари 2:')
 # new_str =  input('Введите строку на английском: ')
-new_str = 'A Long Time Ago In A Galaxy Far Far Away test test A'
+new_str = 'A long time ago, in a galaxy far, far away...\nIt is a period of civil war. Rebel spaceships, \n' \
+          'striking from a hidden base, have won their \nfirst victory against the evil Galactic Empire. \nDuring the battle, ' \
+          'Rebel spies managed to steal \nsecret plans to the Empires ultimate weapon, \nthe Death Star, an armored space station ' \
+          'with \nenough power to destroy an entire planet. Pursued \nby the Empires sinister agents, Princess Leia \nraces home aboard ' \
+          'her starship, custodian \nof the stolen plans that can save her people \nand restore freedom to the galaxy....'
 print('Дана строка: \n' + new_str)
 d = {}
 lst = []
@@ -10,4 +14,4 @@ for word in new_str.split():
 for i in d.items():
     if i[1] == max(d.values()):
         lst = list(i)
-print('\nВ вашей строке чаше всего встречается слово "' + str(lst[0]) + '"')
+print('\nВ вашей строке чаше всего встречается слово "' + str(lst[0]) + '" в количестве "' + str(lst[1]) + '" раз')
