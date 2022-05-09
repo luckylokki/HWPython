4d0987c49c7098163bd34dcf1d568412f462f1f3
loki <mr.vadim.prokopenko@gmail.com>
2022-05-09 11:40:44 +0300

    hw23_done

---
+++ lesson_09/hw23_ledger.py
@@ -0,0 +1,18 @@
+'''
+Напишите программу на Python, которая на вход получает список списков и возвращает список кортежей.
+Каждый кортеж состоит из номера заказа и произведения
+цены на товары и количества. Стоимость заказа должна быть увеличена на $10, если она
+меньше $100. Программа должна использовать lambda, map, однострочный if, round и list.
+'''
+print('ДЗ 23. Бухгалтерская книга\n')
+books = \
+    [
+        [34587, 'Learning Python, Mark Lutz', 4, 40.95],
+        [98762, 'Programming Python, Mark Lutz', 5, 56.80],
+        [77226, 'Head First Python, Paul Barry', 3, 32.95],
+        [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]
+    ]
+print('Список заказов: ')
+print('\n'.join(map(str, books)))
+print('\nРезультат решения задачи: ')
+print(tuple(map(lambda x: x if x[1] >= 100 else (x[0], x[1] + 10), map(lambda x: (x[0], round(x[2] * x[3], 2)), books))))
