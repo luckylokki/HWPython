c9eaf554e9e866ee8ff1e358bb66adef4b4676fc
lokki <44494883+luckylokki@users.noreply.github.com>
2022-05-17 10:51:04 +0300

    hw_25 like this

--- lesson_10/hw25_matrix2.py
+++ lesson_10/hw25_matrix2.py
@@ -5,17 +5,17 @@ strings = int(input('Количество строк: '))
 tables = int(input('Количество столбцов: '))
 matrix = [[randint(10, 99) for j in range(tables)] for i in range(strings)]
 f_matrix = '{matr:<1}{su:>6}'
-suma1 = [0] * tables
-for i in range(strings):
-    suma = 0
-    for j in range(tables):
-        suma += matrix[i][j]
-        suma1[j] += matrix[i][j]
-    print(f_matrix.format(matr=str(matrix[i]), su=suma))
+sum_cols = [0] * tables
 
-for i in range(len(suma1)):
-    if suma1[i] < 100:
-        spl_y = '  '
-    elif suma1[i] > 100:
-        spl_y = ' '
-    print(suma1[i], end=spl_y)
+for i in range(strings):
+    sum_rows = 0
+    for j in range(tables):
+        sum_rows += matrix[i][j]
+        sum_cols[j] += matrix[i][j]
+    print(f_matrix.format(matr=str(matrix[i]), su=sum_rows))
+# print("---" * strings)
+# matrix.append(sum_cols)
+# print(f_matrix.format(matr=str(matrix), su=sum_rows))
+# exit()
+for i in range(len(sum_cols)):
+    print(sum_cols[i], end=' ')
