a731f57866c6dc3caf6fe1031d300fbcc44912a1
lokki <44494883+luckylokki@users.noreply.github.com>
2022-05-19 18:26:04 +0300

    hw_27,28 done

--- lesson_12/triangle1.py
+++ lesson_12/triangle1.py
@@ -1,11 +1,15 @@
 print('ДЗ 27. Треугольник 1')
 rows = int(input('Введите высоту треугольника: '))
+
+
 def triangle(size):
-    for row in range(1,rows+1):
-        for col in range(1,2*rows):
-            if row == rows or row+col == rows+1 or col-row== rows-1:
-                print('*',end=' ')
+    for row in range(1, rows + 1):
+        for col in range(1, 2 * rows):
+            if row == rows or row + col == rows + 1 or col - row == rows - 1:
+                print('*', end=' ')
             else:
                 print(end='  ')
         print()
+
+
 triangle(rows)
--- lesson_12/triangle2.py
+++ lesson_12/triangle2.py
@@ -1,5 +1,10 @@
+print('ДЗ 28. Треугольник 2')
 rows = int(input('Введите высоту треугольника: '))
+
+
 def triangle(size):
     for row in range(size + 1):
-        print(' ' * (size - row) + '*' * (2 * row -1))
+        print('  ' * (size - row) + '* ' * (2 * row - 1))
+
+
 triangle(rows)
