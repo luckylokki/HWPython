cef486c39b644a7d193e1348df3629eb13d518f7
lokki <44494883+luckylokki@users.noreply.github.com>
2022-05-19 17:20:13 +0300

    hw_26 update

--- lesson_11/hw26_converter.py
+++ lesson_11/hw26_converter.py
@@ -2,13 +2,13 @@ from string import ascii_uppercase
 from string import digits
 print('ДЗ 26. Конвертер чисел.')
 symbols = digits + ascii_uppercase
-def converter(nums, system):
+def converter(nums, num_system):
     convert = []
     while nums:
-        convert.insert(0, symbols[nums % system])
-        nums //= system
+        convert.insert(0, symbols[nums % num_system])
+        nums //= num_system
     convert = ''.join(convert)
     return convert
 num = int(input('Введите число: '))
-syst = int(input('Введите систему исчесления от 2 до 36: '))
-print('Результат конвертирования:', converter(num, syst))
+system = int(input('Введите систему исчесления от 2 до 36: '))
+print('Результат конвертирования:', converter(num, system))
