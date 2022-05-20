8e5942a0228dcf07da5968762f25e33b0cd5198e
lokki <44494883+luckylokki@users.noreply.github.com>
2022-05-20 17:05:02 +0300

    hw_29 update done

--- lwsson_13/hw29_files.py
+++ lwsson_13/hw29_files.py
@@ -10,13 +10,11 @@ avr_tt = 0
 for lines_cred in students:
     count += 1
     data_cred = lines_cred.split(' ')
-    surname = data_cred[1]
-    name = data_cred[0]
-    shurn_n = data_cred[1] + ' ' + ''.join([i[0] for i in data_cred[0].split(' ')]) + '.'
+    shurname_name = data_cred[1] + ' ' + ''.join([i[0] for i in data_cred[0].split(' ')]) + '.'
     average = str((round(int(sum(int(x) for x in lines_cred.split(' ')[2:])) / len(lines_cred.split(' ')[2:]), 2)))
     if float(average) < 5.0:
-        print('{shurname_name:<21}{avrg:>4}'.format(shurname_name=shurn_n, avrg=average))
-    new_file.write('{shurname_name:<21}{avrg:>4}\n'.format(shurname_name=shurn_n, avrg=average))
+        print('{shurn_n:<21}{avrg:>4}'.format(shurn_n=shurname_name, avrg=average))
+    new_file.write('{shurn_n:<21}{avrg:>4}\n'.format(shurn_n=shurname_name, avrg=average))
     average_total = (int(sum(int(x) for x in lines_cred.split(' ')[2:])))
     avr_tt += average_total
 print()
