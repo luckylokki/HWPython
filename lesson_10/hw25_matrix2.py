c068412fba657b03eb57d2e5d1ad184e4b01f93d
lokki <44494883+luckylokki@users.noreply.github.com>
2022-05-22 12:21:22 +0300

    hw_25 update done top+

--- lesson_10/hw25_matrix2.py
+++ lesson_10/hw25_matrix2.py
@@ -8,7 +8,6 @@ matrix = [[randint(10, 50) for j in range(cols)] for i in range(rows)]
 sum_cols = [0] * cols
 for i in range(rows):
     sum_rows = 0
-    lst = []
     for j in range(cols):
         sum_rows += matrix[i][j]
         sum_cols[j] += matrix[i][j]
