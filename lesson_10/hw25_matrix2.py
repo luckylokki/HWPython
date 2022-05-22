c9dc04a42b768c765589616b6e44724e9f12ed42
lokki <44494883+luckylokki@users.noreply.github.com>
2022-05-22 09:58:00 +0300

    hw_25 update variables names

--- lesson_10/hw25_matrix2.py
+++ lesson_10/hw25_matrix2.py
@@ -1,18 +1,18 @@
 from random import randint
 print('ДЗ 25. Матрица 2')
 
-strings = int(input('Количество строк: '))
-tables = int(input('Количество столбцов: '))
-matrix = [[randint(10, 99) for j in range(tables)] for i in range(strings)]
-sum_cols = [0] * tables
-for i in range(strings):
+rows = int(input('Количество строк: '))
+columns = int(input('Количество столбцов: '))
+matrix = [[randint(10, 99) for j in range(columns)] for i in range(rows)]
+sum_cols = [0] * columns
+for i in range(rows):
     sum_rows = 0
     lst = []
-    for j in range(tables):
+    for j in range(columns):
         sum_rows += matrix[i][j]
         sum_cols[j] += matrix[i][j]
         lst.append(str(matrix[i][j]))
-    print('{matr:>{x}}{su:>10}'.format(matr='  '.join(lst), su=sum_rows, x=tables * 4-1))
+    print('{matr:>{x}}{su:>10}'.format(matr='  '.join(lst), su=sum_rows, x=columns * 4-1))
 
 for i in range(len(sum_cols)):
     print('{:>3}'.format(sum_cols[i]), end=' ')
