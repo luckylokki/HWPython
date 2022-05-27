9d4449c47fa7aa569781c84e624a5d1306f407a4
lokki <44494883+luckylokki@users.noreply.github.com>
2022-05-27 17:11:06 +0300

    hw_30 29update

--- lesson_14/hw_30_counter.py
+++ lesson_14/hw_30_counter.py
@@ -1,8 +1,6 @@
 '''
 Реализовать класс цифрового счетчика. В классе реализовать методы:
-установки максимального, минимального и начального значения счётчика
- (предусмотреть установку 
-счётчика значениями по умолчанию 0-100)
+установки максимального, минимального и начального значения счётчика (предусмотреть установку счётчика значениями по умолчанию 0-100)
 увеличения счетчика на 1
 возвращения текущего значения счётчика
 '''
@@ -15,31 +13,25 @@ class Counter:
     def condition(self):
         if self.start < self.end:
             self.start += 1
-        return self.start
+            return self.start
+        else:
+            return 'Out of range'
 
 print('ДЗ 30. Цифровой счётчик\n')
-print('Введите минимальное или максимальное значение счетчика\nЕсли хотите \
-воспользоватся стандартным значением просто нажмите Enter')
-print()
 
-start_num = input("Введите минимальное значение счетчика: ")
-end_num = input("Введите максимальное значение счетчика: ")
-
+a = input("Please, enter start: ")
+b = input("Please, enter end: ")
 cnt = Counter()
-
-if start_num == '':
-    start_num = cnt.start
+if a == '':
+    a = cnt.start
 else:
-    start_num = int(start_num)-1
-if end_num == '':
-    end_num = cnt.end
+    a = int(a)-1
+if b == '':
+    b = cnt.end
 else:
-    end_num = int(end_num)
+    b = int(b)
 
-if start_num > end_num:
-	print('Стартовое число не может быть меньше финального')
-
-data = Counter(start=start_num, end=end_num)
-
-for i in range(start_num, end_num):
-    print(data.condition())
+count = Counter(start=a, end=b)
+# print(a)
+for i in range(a, b):
+    print(count.condition())
