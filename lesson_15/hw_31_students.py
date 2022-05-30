55d5c3e9711c7e83d67ab4b88fb6bfdc17b48c06
luckylokki <44494883+luckylokki@users.noreply.github.com>
2022-05-30 22:46:50 +0300

    metod classa

--- lesson_15/hw_31_students.py
+++ lesson_15/hw_31_students.py
@@ -1,51 +1,51 @@
 class Student:
-    def __init__(self, name):
-        self.name = name
-        self.grades = []
+	def __init__(self, name):
+		self.name = name
+		self.grades = []
 
-    def add_grade(self, grade):
-        self.grade = grade
-        self.grades.append(grade)
+	def add_grade(self, grade):
+		self.grade = grade
+		self.grades.append(grade)
 
-    def get_grades(self):
-        return self.grades
+	def get_grades(self):
+		return self.grades
 
-    def __str__(self):
-        return 'Студент: {n:<10} Оценки: {g}'.format(
-            n=self.name,
-            g=' '.join(str(grade) for grade in self.grades)
-        )
+	def __str__(self):
+		return 'Студент: {n:<10} Оценки: {g}'.format(
+			n=self.name,
+			g=' '.join(str(grade) for grade in self.grades)
+		)
 
 
 class Group:
-    def __init__(self):
-        self.name = input('Введите имя группы: ')
-        self.students = []
+	def __init__(self):
+		self.amount = int(input('Сколько студентов вы хотите добавить?: '))
+		self.name = input('Введите имя группы: ')
+		self.students = []
 
-    def add_student(self, student):
-        st = Student(student.name)
-        for grade in student.get_grades():
-            st.add_grade(grade)
-        self.students.append(st)
+	def add_student(self, student):
+		st = Student(student.name)
+		for grade in student.get_grades():
+			st.add_grade(grade)
+		self.students.append(st)
 
-    def print_group(self):
-        print(f'Имя группы: {self.name}')
-        for student in self.students:
-            print(student)
+	def print_group(self):
+		print(f'Имя группы: {self.name}')
+		for student in self.students:
+			print(student)
 
-    def amount_students(self, amount):
-        from random import randint
-        self.amount = amount
-        for x in range(1, self.amount + 1):
-            st_c = Student(input(f'Введите имя {x} студента: '))
-            for i in range(10):
-                st_c.add_grade(randint(1, 9))
-            self.add_student(st_c)
+	def amount_students(self):
+		from random import randint
+		for x in range(1, self.amount + 1):
+			st_c = Student(input(f'Введите имя {x} студента: '))
+			for i in range(10):
+				st_c.add_grade(randint(1, 9))
+			self.add_student(st_c)
 
 
-students_amount = int(input('Сколько студентов вы хотите добавить?: '))
+
 
 gr = Group()
-gr.amount_students(amount=students_amount)
+gr.amount_students()
 print()
 gr.print_group()
